717aaf857bfd5c3c0035922bf144d2407755049e
ItsFire143 <112923603+ItsFire143@users.noreply.github.com>
2022-09-09 16:38:58 +0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,7 +1,8 @@
 public class Main {
 
     public static void main(String[] args) {
-
+        
+        public static string ChekWords() { 
         String a = "Ketchup";
         String b = "Ketchup";
         String c = "Kepchuk";
@@ -18,7 +19,20 @@ public class Main {
         {
             System.out.println("Слово " + a + " неверно");
         };
-
     }
+    // задание 2
+    public static float calcSum (int[] numbersArray, int a, int b, int di) {
+        int resultA = 0; // числитель итог
+        int resultB = 0; // знаменатель итог
+        int resultSumma = 0; // Итог
 
+        for (int i = 0; i < 7; i++) {
+            resultA += number - a*b;
+        }
+        for (int i = 0; i < 5; i++) {
+            resultB += number - a;
+        }
+        return (float) (resultA / resultB) * numbersArray[di - 1];
+        System.out.println(r);
+    }
 }
